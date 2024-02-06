38cb838547c60f5ae180d2a11541a4923d5547e5
Raul Cotar <raulcotar00@gmail.com>
2024-02-06 17:42:01 +0200

    New train gfx, small fixes, cargotable updates

--- trainXtensionGRF/build.sh
+++ trainXtensionGRF/build.sh
@@ -1,3 +1,3 @@
-gcc -xc -P -nostdinc -E src/trainXTension.pnml -D_VER_NR=32 -D_VER_COMP_NR=31 -o src/final.nml
-../nml/nmlc -l lang --grf trainXTension.grf src/final.nml
+gcc -xc -P -nostdinc -E src/trainXTension.pnml -D_VER_NR=33 -D_VER_COMP_NR=31 -o src/final.nml
+../nml-jgrpp-git/nmlc -l lang --grf trainXTension.grf src/final.nml
 cp -i trainXTension.grf ~/.local/share/openttd-jgrpp/newgrf/
